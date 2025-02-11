498bbd2f72b1dc060d04ed357fe84c866f3f8dd3
Timo Notheisen <timo.notheisen@js-soft.com>
2025-02-11 13:13:15 +0100

    Revert "refactor: move appDir getter to publicly accessible function"
    This reverts commit 68b663d7c377a3f3a60322fdb165ec5587b62ca9.

--- src/TUI.ts
+++ src/TUI.ts
@@ -4,7 +4,6 @@ import path from "path"
 import prompts from "prompts"
 import { Config } from "./connector-config/Config.js"
 import { TUIBaseWithMixins } from "./mixins/TUIBaseWithMixins.js"
-import { getAppDir } from "./utils/getAppDir.js"
 
 export class TUI extends TUIBaseWithMixins {
   public constructor(private readonly settings: { dashboard: boolean }) {
@@ -14,7 +13,7 @@ export class TUI extends TUIBaseWithMixins {
   public async run() {
     await this.showStartupMessage()
 
-    await Config.load(path.join(getAppDir(), "config.json"))
+    await Config.load(path.join(this.appDir, "config.json"))
 
     await this.connectToPM2()
     this.scheduleKillTask()
@@ -52,7 +51,7 @@ export class TUI extends TUIBaseWithMixins {
 
     console.log(`Welcome to the ${chalk.blue("enmeshed Connector Manager TUI")}!`)
     console.log(`TUI Version: ${chalk.yellow(packageJson.version)}`)
-    console.log(`Storing files in: ${chalk.yellow(getAppDir())}`)
+    console.log(`Storing files in: ${chalk.yellow(this.appDir)}`)
     console.log("")
   }
 
--- src/mixins/AddCreateConnector.ts
+++ src/mixins/AddCreateConnector.ts
@@ -34,7 +34,7 @@ export function AddCreateConnector<TBase extends TUIBaseConstructor>(Base: TBase
 
       if (!selected.release) return
 
-      await installConnector(getAppDir(), selected.release.downloadUrl, selected.release.version)
+      await installConnector(this.appDir, selected.release.downloadUrl, selected.release.version)
     }
 
     private async getInstallableReleases() {
