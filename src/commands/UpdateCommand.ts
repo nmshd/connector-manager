e6d51555dd4833af31eee60dcc231aeec4198e72
Julian König <julian.koenig@js-soft.com>
2025-02-12 09:26:31 +0100

    feat: add noop update command

---
+++ src/commands/UpdateCommand.ts
@@ -0,0 +1,48 @@
+import chalk from "chalk"
+import * as yargs from "yargs"
+import { BaseCommand } from "./BaseCommand.js"
+
+export type UpdateCommandArgs = { force: boolean; version: string } & ({ name: string } | { all: true })
+
+export class UpdateCommand extends BaseCommand<never> {
+  public static builder: yargs.BuilderCallback<any, never> = (yargs: yargs.Argv) =>
+    yargs
+      .option("name", { type: "string" })
+      .option("all", { type: "boolean" })
+      .option("force", { type: "boolean" })
+      .option("version", { type: "string", requiresArg: true })
+      .conflicts("name", "all")
+      .check((argv) => {
+        if (!("name" in argv || "all" in argv)) return "Either --name or --all must be provided."
+        return true
+      })
+
+  protected async runInternal(args: UpdateCommandArgs): Promise<void> {
+    const existsResponse = await this._releaseManager.exists(args.version)
+    if (existsResponse) {
+      console.error(existsResponse)
+      process.exit(1)
+    }
+
+    if ("all" in args) {
+      for (const connector of this._config.connectors) {
+        await this.update(connector.name)
+      }
+
+      return
+    }
+
+    if (!this._config.connectors.find((c) => c.name === args.name)) {
+      console.error(`A connector with the name ${chalk.red(args.name)} does not exist.`)
+      process.exit(1)
+    }
+
+    await this.update(args.name)
+  }
+
+  private async update(name: string) {
+    console.log(`Updating connector ${chalk.green(name)}...`)
+
+    await Promise.resolve()
+  }
+}
