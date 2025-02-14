8a94ef9dfa360096b182985ddcb9ab5640213fcb
Timo Notheisen <timo.notheisen@js-soft.com>
2025-02-14 08:19:06 +0100

    feat: improve CreateCommand documentation

--- src/commands/CreateCommand.ts
+++ src/commands/CreateCommand.ts
@@ -14,15 +14,35 @@ export interface CreateCommandArgs {
 export class CreateCommand extends BaseCommand<CreateCommandArgs> {
   public static builder: yargs.BuilderCallback<any, CreateCommandArgs> = (yargs: yargs.Argv) =>
     yargs
-      .option("name", { type: "string", demandOption: true })
-      .option("version", { type: "string", demandOption: true })
-      .option("db-connection-string", { type: "string" })
-      .option("base-url", { type: "string" })
-      .option("client-id", { type: "string" })
-      .option("client-secret", { type: "string" })
+      .option("name", {
+        type: "string",
+        demandOption: true,
+        description: "The name of the connector to create. Must be unique, contain only lowercase characters and must not contain any whitespace characters.",
+      })
+      .option("version", {
+        type: "string",
+        demandOption: true,
+        description: "The version of the connector to create. You can find a list of available versions on https://github.com/nmshd/connector/releases.",
+      })
+      .option("db-connection-string", {
+        type: "string",
+        description: "The connection string for the database the connector should use. Defaults to the value you specified during 'cman init'.",
+      })
+      .option("base-url", { type: "string", description: "The base URL of the backbone the connector should connect to. Defaults to the value you specified during 'cman init'." })
+      .option("client-id", {
+        type: "string",
+        description:
+          "The client ID of the OAuth2 client that should be used to authenticate the Connector on the Backbone. Defaults to the value you specified during 'cman init'.",
+      })
+      .option("client-secret", {
+        type: "string",
+        description:
+          "The client secret of the OAuth2 client that should be used to authenticate the Connector on the Backbone. Defaults to the value you specified during 'cman init'.",
+      })
       .check((argv) => {
-        if (argv.name === "all") return "The name 'all' is reserved."
         if (argv.name.trim().length === 0) return "The name cannot be empty."
+        if (argv.name.toLowerCase() !== argv.name) return "The name must be all lowercase."
+        if (/\s/.test(argv.name)) return "The name must not contain any whitespace characters."
         return true
       })
 
