33be5910f63474a4e4536dd71b796b9a4179e41d
Timo Notheisen <timo.notheisen@js-soft.com>
2025-02-14 17:01:07 +0100

    feat: improve excel sync

--- src/commands/excelCommands/ExcelSyncCommand.ts
+++ src/commands/excelCommands/ExcelSyncCommand.ts
@@ -1,5 +1,6 @@
 import xlsx from "node-xlsx"
 import * as yargs from "yargs"
+import { setDisplayName } from "../../utils/connectorUtils.js"
 import { BaseCommand } from "../BaseCommand.js"
 
 export interface ExcelSyncCommandArgs {
@@ -20,6 +21,8 @@ export class ExcelSyncCommand extends BaseCommand<ExcelSyncCommandArgs> {
 
     const data = this.convertToJSON(workSheet.data)
 
+    await this.deleteOrphanedConnectors(data)
+
     for (const connectorProperties of data) {
       if (this._config.existsConnector(connectorProperties.name)) {
         await this.updateExistingConnector(connectorProperties)
@@ -28,42 +31,56 @@ export class ExcelSyncCommand extends BaseCommand<ExcelSyncCommandArgs> {
       }
     }
 
-    await this.deleteOrphanedConnectors(data)
-
     await this._config.save()
+
+    await new Promise((resolve) => setTimeout(resolve, 1000 * this._config.connectors.length))
 
     await this.showInstances(this._config.connectors)
   }
 
-  private async updateExistingConnector(connectorProperties: Parameters) {
-    const connector = this._config.getConnector(connectorProperties.name)!
+  private async updateExistingConnector(parameters: Parameters) {
+    console.log(`Updating connector ${parameters.name}...`)
 
-    connector.version = connectorProperties.version
-    connector.config.database.connectionString = connectorProperties["db-connection-string"] ?? this._config.dbConnectionString
-    connector.config.transportLibrary.baseUrl = connectorProperties["base-url"] ?? this._config.platformBaseUrl
-    connector.config.transportLibrary.platformClientId = connectorProperties["client-id"] ?? this._config.platformClientId
-    connector.config.transportLibrary.platformClientSecret = connectorProperties["client-secret"] ?? this._config.platformClientSecret
+    const connector = this._config.getConnector(parameters.name)!
+
+    connector.version = parameters.version
+    connector.config.database.connectionString = parameters["db-connection-string"] ?? this._config.dbConnectionString
+    connector.config.transportLibrary.baseUrl = parameters["base-url"] ?? this._config.platformBaseUrl
+    connector.config.transportLibrary.platformClientId = parameters["client-id"] ?? this._config.platformClientId
+    connector.config.transportLibrary.platformClientSecret = parameters["client-secret"] ?? this._config.platformClientSecret
+    connector.config.infrastructure.httpServer.port = parameters.port ? parseInt(parameters.port) : connector.config.infrastructure.httpServer.port
+
+    await this._config.save()
 
     await this._processManager.restart(connector.name)
   }
 
   private async createNewConnector(parameters: Parameters) {
+    console.log(`Creating connector ${parameters.name}...`)
+
     const connector = this._config.addConnector(
       parameters.version,
       parameters.name,
       parameters["db-connection-string"],
       parameters["base-url"],
       parameters["client-id"],
-      parameters["client-secret"]
+      parameters["client-secret"],
+      parameters.port ? parseInt(parameters.port) : undefined
     )
 
+    await this._config.save()
+
     await this._processManager.start(connector.name)
+
+    if (parameters["initial-display-name"]) await setDisplayName(connector, parameters["initial-display-name"].trim())
   }
 
   private async deleteOrphanedConnectors(parameters: Parameters[]) {
     const connectorsWithNoMatchingRowInExcel = this._config.connectors.filter((c) => !parameters.some((d) => d.name === c.name))
 
     for (const connectorParameters of connectorsWithNoMatchingRowInExcel) {
+      console.log(`Deleting orphaned connector ${connectorParameters.name}...`)
+
       const status = await this._processManager.status(connectorParameters.name)
 
       if (status[0]?.pid) {
@@ -100,6 +117,8 @@ export class ExcelSyncCommand extends BaseCommand<ExcelSyncCommandArgs> {
 class Parameters {
   public "name" = ""
   public version = ""
+  public port?: string
+  public "initial-display-name"?: string
   public "base-url"?: string
   public "client-id"?: string
   public "client-secret"?: string
